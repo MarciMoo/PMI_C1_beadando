65698fa5916b61935019873ae442f6192ef55638
Schwartz Ádám <99468829+MarciMoo@users.noreply.github.com>
2022-05-09 09:11:39 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,19 +22,32 @@ public class Main {
         lehetosegek();
     }
     public static void lehetosegek(){
-        int i = 0;
-        out.println("\nLehetőségek: \n1: Betegek listázása.\n2: Beteg felvétele.\n3: Beteg levétele.\n4: Módosítás.\n5: Keresés.\n6: Mentés.\n7: Mentés és Kilépés.\n");
+        out.println("\nLehetőségek: \n1: Betegek listázása.\n2: Beteg felvétele.\n3: Beteg törlése.\n4: Módosítás.\n5: Keresés.\n6: Mentés.\n7: Mentés és Kilépés.\n");
         out.print("A Döntés: ");
-        i = sc.nextInt();
-        switch (i) {
-            case 1: listazas();
-            case 2: hozzaad();
-            case 3: torles();
-            case 4: frissites();
-            case 5: kereses();
-            case 6: savePatientsToXml(betegek, "src/main/resources/patients.xml");
-            case 7: kilepes = 65; savePatientsToXml(betegek, "src/main/resources/patients.xml");
-            default: out.println("\nA LEHETŐSÉGEK KÖZÜL KELL VÁLASZTANI!\n");enter();lehetosegek();
+        String a = sc.nextLine();
+        if(a.equals("1")){
+            listazas();
+        }
+        else if(a.equals("2")){
+            hozzaad();
+        }
+        else if(a.equals("3")){
+            torles();
+        }
+        else if(a.equals("4")){
+            frissites();
+        }
+        else if(a.equals("5")){
+            kereses();
+        }
+        else if(a.equals("6")){
+            savePatientsToXml(betegek, "src/main/resources/patients.xml");
+        }
+        else if(a.equals("7")){
+            kilepes = 65; savePatientsToXml(betegek, "src/main/resources/patients.xml");
+        }
+        else {
+            out.println("\nA LEHETŐSÉGEK KÖZÜL KELL VÁLASZTANI!\n");enter();lehetosegek();
         }
     }
     public static void enter(){
@@ -54,9 +67,6 @@ public class Main {
         lehetosegek();
     }
     public static void hozzaad(){
-        out.print("");
-        String miert = sc.nextLine();
-        //ezekre az üres bekérésekre gondoltam a readme-ben
         out.print("Beteg azonosítója: ");
         String taj = sc.nextLine();
         out.print("Beteg neve: ");
@@ -74,8 +84,6 @@ public class Main {
         lehetosegek();
     }
     public static void torles(){
-        out.print("");
-        String miert = sc.nextLine();
         out.println("Eltávolítandó beteg azonosítója: ");
         String i = sc.nextLine();
         int k = 0;
@@ -95,67 +103,62 @@ public class Main {
         }
     }
     public static void frissites(){
-        out.print("\nFrissítendő beteg sorszáma: ");
-        int i = sc.nextInt() - 1;
-        patient beteg = betegek.get(i);
-        out.print("\nA beteg frissítendő adata: \n 1: Azonosítója\n 2: Neve\n 3: Születési dátum\n 4: Kezelés kezdete\n 5: Betegségének neve\n");
-        int j = sc.nextInt();
-        switch (j){
-            case 1: frissitesid(beteg);
-            case 2: frissitesnev(beteg);
-            case 3: frissitesszuli(beteg);
-            case 4: frissiteskezd(beteg);
-            case 5: frissitessugma(beteg);
+        out.print("\nFrissítendő beteg azonosítója: ");
+        String i = sc.nextLine();
+        int k = 0;
+        for(int j = 0; j < betegek.size(); j++){
+            if(betegek.get(j).getTaj().equals(i)) {
+                patient beteg = betegek.get(j);
+                out.print("\nA beteg frissíthető adatai: \n 1: Azonosítója\n 2: Neve\n 3: Születési dátum\n 4: Kezelés kezdete\n 5: Betegségének neve\nVálasztás: ");
+                String b = sc.nextLine();
+                if(b.equals("1")){
+                    out.print("A módosítás: ");
+                    String x = sc.nextLine();
+                    beteg.setTaj(x);
+                    enter();
+                    lehetosegek();
+                }
+                else if(b.equals("2")){
+                    out.print("A módosítás: ");
+                    String x = sc.nextLine();
+                    beteg.setName(x);
+                    enter();
+                    lehetosegek();
+                }
+                else if(b.equals("3")){
+                    out.print("A módosítás: ");
+                    String x = sc.nextLine();
+                    beteg.setBirthYear(x);
+                    enter();
+                    lehetosegek();
+                }
+                else if(b.equals("4")){
+                    out.print("A módosítás: ");
+                    String x = sc.nextLine();
+                    beteg.setKezeleskezdete(x);
+                    enter();
+                    lehetosegek();
+                }
+                else if(b.equals("5")){
+                    out.print("A módosítás: ");
+                    String x = sc.nextLine();
+                    beteg.setBetegsegneve(x);
+                    enter();
+                    lehetosegek();
+                }
+                else{
+                    out.println("\nA LEHETŐSÉGEK KÖZÜL KELL VÁLASZTANI!\n");enter();lehetosegek();
+                }
+            }
+            else if(j == (betegek.size()-1) && k != 1){
+                out.println("\nBETEG SIKERTELENÜL MÓDOSÍTVA!\nNINCS TALÁLAT A BETEGRE\n");
+                enter();
+                lehetosegek();
+            }
         }
-    }
-    public static void frissitesid(patient beteg){
-        out.print("");
-        String miert = sc.nextLine();
-        out.print("A módosítás: ");
-        String b = sc.nextLine();
-        beteg.setTaj(b);
-        enter();
-        lehetosegek();
-    }
-    public static void frissitesnev(patient beteg){
-        out.print("");
-        String miert = sc.nextLine();
-        out.print("A módosítás: ");
-        String b = sc.nextLine();
-        beteg.setName(b);
-        enter();
-        lehetosegek();
-    }
-    public static void frissitesszuli(patient beteg){
-        out.print("");
-        String miert = sc.nextLine();
-        out.print("A módosítás: ");
-        String b = sc.nextLine();
-        beteg.setBirthYear(b);
-        enter();
-        lehetosegek();
-    }
-    public static void frissiteskezd(patient beteg){
-        out.print("");
-        String miert = sc.nextLine();
-        out.print("A módosítás: ");
-        String b = sc.nextLine();
-        beteg.setKezeleskezdete(b);
-        enter();
-        lehetosegek();
-    }
-    public static void frissitessugma(patient beteg){
-        out.print("");
-        String miert = sc.nextLine();
-        out.print("A módosítás: ");
-        String b = sc.nextLine();
-        beteg.setBetegsegneve(b);
-        enter();
-        lehetosegek();
+
     }
     public static void kereses(){
-        out.print("");
-        String miert = sc.nextLine();
         out.println("Keresett beteg azonosítója: ");
         String i = sc.nextLine();
         int k = 0;
@@ -180,11 +183,9 @@ public class Main {
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(filepath);
-
             Element rootElement = document.getDocumentElement();
             NodeList childNodesList = rootElement.getChildNodes();
             Node node;
-
             for (int i = 0; i < childNodesList.getLength(); i++) {
                 node = childNodesList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
@@ -216,30 +217,21 @@ public class Main {
             Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
             Element rootElement = document.createElement("betegek");
             document.appendChild(rootElement);
-
-
             for (patient beteg : betegek) {
                 Element beteg1 = document.createElement("beteg");
                 rootElement.appendChild(beteg1);
-
                 createChildElement(document, beteg1, "taj", beteg.getTaj());
                 createChildElement(document, beteg1, "name", beteg.getName());
                 createChildElement(document, beteg1, "birthYear",beteg.getBirthYear());
                 createChildElement(document, beteg1, "kezeleskezdete",beteg.getKezeleskezdete());
                 createChildElement(document, beteg1, "betegsegneve", beteg.getBetegsegneve());
             }
-
-
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
-
             DOMSource source = new DOMSource(document);
             StreamResult result = new StreamResult(new FileOutputStream(filepath));
-
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.transform(source, result);
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
